58cc8cd203cc574714d22989b4b85a1c5a5dcf40
rafajlpz <frilo_94@hotmail.com>
2022-02-13 18:32:32 +0100

    Editar BBDD
    -Uso del boton editar
    -Aplicacion de async y await para la modificacion

--- src/store/index.js
+++ src/store/index.js
@@ -80,9 +80,22 @@ export default createStore({
     setTarea({ commit }, id) {
       commit("tarea", id);
     },
-    updateTarea({ commit }, tarea) {
-      commit("update", tarea);
-    },
+    async updateTarea({ commit }, tarea) {
+      try {
+        const res = await fetch(
+          `https://udemy-api-8ec44-default-rtdb.europe-west1.firebasedatabase.app/tareas/
+          ${tarea.id}.json`,
+          {
+            method: "PATCH",
+            body: JSON.stringify(tarea)
+          }
+        )
+        const dataDB = await res.json()
+        commit("update", dataDB);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   modules: {},
 });
